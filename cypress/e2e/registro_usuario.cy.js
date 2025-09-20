1fc9af0ecf2e2e95a885531203a12ed084372467
Ornella <ornellalezcano1100@gmail.com>
2025-09-20 13:31:24 -0300

    feat: Agrego casos de prueba de registro (positivo y negativo)

--- cypress/e2e/registro_usuario.cy.js
+++ cypress/e2e/registro_usuario.cy.js
@@ -1,4 +1,18 @@
 describe('Automatización del registro de usuario', () => {
+
+    // Objeto con los datos de prueba
+    const userData = {
+        nombres: 'Metallica',
+        apellido: 'Test S.A',
+        telefono: '3517294401',
+        provincia: 'Buenos Aires',
+        localidad: 'San Isidro',
+        diaNacimiento: '17',
+        mesNacimiento: '10',
+        anioNacimiento: '2001',
+        password: 'Metal_Test#2001'
+    };
+    
     // Antes de cada test, visitamos la página de registro
     beforeEach(() => {
         cy.visit('https://ticketazo.com.ar/auth/registerUser');
@@ -10,29 +24,29 @@ describe('Automatización del registro de usuario', () => {
         const randomDni = `${Math.floor(10000000 + Math.random() * 90000000)}`;
 
         // Paso 1: Localizar los campos e introducir los datos
-        cy.get('[data-cy="input-nombres"]').type('Metallica');
-        cy.get('[data-cy="input-apellido"]').type('Test S.A');
-        cy.get('[data-cy="input-telefono"]').type('3517294401');
-        cy.get('[data-cy="input-dni"]').type('45.200.199');
+        cy.get('[data-cy="input-nombres"]').type(userData.nombres);
+        cy.get('[data-cy="input-apellido"]').type(userData.apellido);
+        cy.get('[data-cy="input-telefono"]').type(userData.telefono);
+        cy.get('[data-cy="input-dni"]').type(randomDni);
 
         // Seleccionar Provincia y Localidad
         cy.get('[data-cy="select-provincia"]').click();
-        cy.contains('Buenos Aires').click();
-        cy.get('[data-cy="select-localidad"]').type('San Isidro');
-        cy.contains('San Isidro').click();
+        cy.contains(userData.provincia).click();
+        cy.get('[data-cy="select-localidad"]').type(userData.localidad);
+        cy.contains(userData.localidad).click();
 
         // Ingresar la fecha de nacimiento
-        cy.get('[data-type="day"]').type('17');
-        cy.get('[data-type="month"]').type('10');
-        cy.get('[data-type="year"]').type('2001');
+        cy.get('[data-type="day"]').type(userData.diaNacimiento);
+        cy.get('[data-type="month"]').type(userData.mesNacimiento);
+        cy.get('[data-type="year"]').type(userData.anioNacimiento);
 
         // Usar la variable de email para ambos campos
         cy.get('[data-cy="input-email"]').type(randomEmail);
         cy.get('[data-cy="input-confirmar-email"]').type(randomEmail);
 
         // Ingresar y confirmar la contraseña
-        cy.get('[data-cy="input-password"]').type('Metal_Test#2001');
-        cy.get('[data-cy="input-repetir-password"]').type('Metal_Test#2001');
+        cy.get('[data-cy="input-password"]').type(userData.password);
+        cy.get('[data-cy="input-repetir-password"]').type(userData.password);
 
         // Paso 2: Hacer clic en el botón de registro
         cy.get('button[type="submit"]').click();
